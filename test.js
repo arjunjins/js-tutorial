e67e7905e867bb8b837c01ee573c729305bf8586
Arjun J <arjunjins2001@gmail.com>
2024-02-16 12:02:37 +0530

    api initial commit

--- test.js
+++ test.js
@@ -1,11 +1,8 @@
-let codes = {
-  "+49.4": "Germany",
-  "+41": "Switzerland",
-  "+44": "Great Britain",
-  // ..,
-  "+1": "USA",
+let user = {
+  name: "Arjun",
+  ref() {
+    return this;
+  },
 };
 
-for (let code in codes) {
-  console.log(+code); // 49, 41, 44, 1
-}
+console.log(user.ref().name);
